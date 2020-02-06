4432099fe4849d68c581e22167b3b37ad749ce37
humberto <humberto.marcolino@stoodi.com.br>
2020-02-06 11:49:52 -0300

    fixing setup

--- setup.py
+++ setup.py
@@ -1,14 +1,14 @@
-from distutils.core import setup
+rom distutils.core import setup
 setup(
     name = 'json_dict_comparator',
     packages = ['json_dict_comparator'],
-    version = '0.0.2',
+    version = '0.0.3',
     license='MIT',
     description = 'Provides tools for comparing python dictionaries obtained from json objects.',
     author = 'Humberto Cezar',
     author_email = 'betoncios@gmail.com',
     url = 'https://github.com/humbcezar/json-dict-comparator',
-    download_url = 'https://github.com/humbcezar/json-dict-comparator/archive/0.0.2.tar.gz',
+    download_url = 'https://github.com/humbcezar/json-dict-comparator/archive/0.0.3.tar.gz',
     keywords = ['json', 'dict', 'comparison'],
     classifiers=[
         'Development Status :: 3 - Alpha',
